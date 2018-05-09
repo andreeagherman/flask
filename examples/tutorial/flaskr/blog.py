310e2b138a584dce4ac22186fb13644b3b935c48
Andreea Gherman <andreea@passfort.com>
2018-05-09 13:55:45 +0100

    Add extra method

--- examples/tutorial/flaskr/blog.py
+++ examples/tutorial/flaskr/blog.py
@@ -76,6 +76,33 @@ def create():
     return render_template('blog/create.html')
 
 
+@bp.route('/duplicate-create', methods=('GET', 'POST'))
+@login_required
+def dupllicate_create():
+    """Create a new post for the current user."""
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
+
+
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
